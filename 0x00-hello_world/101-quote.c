35ef5275f88b17cfc24bb41f19017fc69165c401
Lanze-001 <lanzelaban@gmail.com>
2022-06-02 13:55:25 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Prints to standard error
+ (*
+ *Description: This prints a statement to standard error)?
+ *Return: One
+ */
+int main(void)
+{
+char txt[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+fwrite(txt, 59, 1, stderr);
+return (1);
+}
