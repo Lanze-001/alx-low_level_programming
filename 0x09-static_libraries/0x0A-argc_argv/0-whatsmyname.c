be2cd3893d388df8c925d1e9362cecfd574721ce
Lanze-001 <lanzelaban@gmail.com>
2022-06-24 10:20:59 -0700

    update

--- 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
+++ 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,23 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - print name of program
- * @argc: argument counter
- * @argv: pointer to array of arguments
- * Return: 0 on success
+ * _strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ * @dest: String that will be appended
+ * @src: String to be concatenated upon
+ *
+ * Return: returns poiner to @dest
  */
 
-int main(int argc, char *argv[])
+char *_strcat(char *dest, char *src)
 {
-	if (argc >= 0)
-		printf("%s\n", argv[0]);
+	int index = 0, dest_len = 0;
 
-	return (0);
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
