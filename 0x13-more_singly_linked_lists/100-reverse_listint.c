a5fdeaddfda3f58871752afac2506a63863d4569
Lanze-001 <lanzelaban@gmail.com>
2022-07-19 08:30:30 -0700

    advanced msll

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - defines the function.
+ * @head: describes the argument.
+ * Return: A pointer to the first node of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	head++;
+	return (0);
+}
